322ce1b3f0553481a15e5a3c6b570fbd81e5f1a4
feliun <felipe.polo@gmail.com>
2017-05-29 19:39:31 +0200

    Tidy up

--- command/command.js
+++ command/command.js
@@ -3,23 +3,23 @@ const R = require('ramda');
 // The command
 const createTextEditorCmd = R.curry((editor, key) => {
 
-    const backspace = '0x7f';
-    let previous = '';
+  const backspace = '0x7f';
+  let previous = '';
 
-    const run = () => {
-        previous = editor.currentText();
-        //strategy pattern
-        key === backspace ? editor.deleteOne() : editor.add(key)
-    };
+  const run = () => {
+      previous = editor.currentText();
+      //strategy pattern
+      key === backspace ? editor.deleteOne() : editor.add(key)
+  };
 
-    const undo = () => {
-        if (key === backspace) return editor.replace(previous);
-        editor.deleteOne();
-    }
+  const undo = () => {
+      if (key === backspace) return editor.replace(previous);
+      editor.deleteOne();
+  }
 
-    const serialise = () => ({ key, current: editor.currentText() });
+  const serialise = () => ({ key, current: editor.currentText() });
 
-    return { run, undo, serialise };
+  return { run, undo, serialise };
 });
 
 module.exports = createTextEditorCmd;
--- command/target.js
+++ command/target.js
@@ -3,24 +3,24 @@ const R = require('ramda');
 // The target
 const createDocEditor = () => {
 
-    let typedText = '';
+  let typedText = '';
 
-    const add = (newChar, text) => text + newChar;
+  const add = (newChar, text) => text + newChar;
 
-    const remove = (text) => R.dropLast(1, text);
+  const remove = (text) => R.dropLast(1, text);
 
-    return {
-        add: (newChar) => {
-            typedText = add(newChar, typedText);
-        },
-        deleteOne: () => {
-            typedText = remove(typedText);
-        },
-        replace: (text) => {
-            typedText = text;
-        },
-        currentText: () => typedText
-    };
+  return {
+    add: (newChar) => {
+        typedText = add(newChar, typedText);
+    },
+    deleteOne: () => {
+        typedText = remove(typedText);
+    },
+    replace: (text) => {
+        typedText = text;
+    },
+    currentText: () => typedText
+  };
 };
 
 module.exports = createDocEditor;
